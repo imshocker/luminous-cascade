2bbc61a0d71d7529ba4653396b0aa7d444170c05
Shocker <zacsmcdowell@gmail.com>
2023-11-01 04:33:10 -0400

    reformatting server.js, fixed syntax within seeds.js

--- seed/seeds.sql
+++ seed/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO department (name)
+INSERT INTO employee_db.department (name)
 VALUES 
 ('Sales'),
 ('Engineering'), 
 ('Finance'),  
 ('Legal');
 
-INSERT INTO role (title, salary, department_id )
+INSERT INTO employee_db.role (title, salary, department_id )
 VALUES 
 /*1*/('Salesperson', 75000, 1),
 /*2*/('Sales Lead', 950000, 1),
@@ -14,10 +14,10 @@ VALUES
 /*5*/('Senior Engineer', 120000, 2),
 /*6*/('Accountant', 97000, 3),
 /*7*/('Account Manager', 130000, 3),
-/*8*/('Lawyer', 150000, 4);
-/*9*/('Legal Team Lead', 185000, 4),
+/*8*/('Lawyer', 150000, 4),
+/*9*/('Legal Team Lead', 185000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee_db.employee (first_name, last_name, role_id, manager_id)
 VALUES 
 ('Jason', 'Bonilla', 1, NULL),
 ('Krista', 'Macntyre', 1, NULL),
@@ -25,7 +25,7 @@ VALUES
 ('George', 'Lucas', 3, 1),
 ('Adrian', 'Birds', 4, 2),
 ('Sierra', 'Mills', 5, NULL),
-('Christopher', 'Hermie', 6, 3);
-('Harmony', 'Tilly', 7, 7);
-('Felicity', 'Hopwell', 8, NULL);
+('Christopher', 'Hermie', 6, 3),
+('Harmony', 'Tilly', 7, 7),
+('Felicity', 'Hopwell', 8, NULL),
 ('Thomas', 'Gonzalez', 9, 9);
--- server.js
+++ server.js
@@ -1,73 +1,96 @@
 const inquirer = require('inquirer')
-const sql = require('mysql2')
-const table = require('console.table');
+require('console.table');
+const mysql = require('mysql2')
 const connection = require('./config/connection');
 
 
+const init = async () => {
+    console.log(`
+    ──────███─█───█─████─█───████─██─██─███─███──────
+    ──────█───██─██─█──█─█───█──█──███──█───█────────
+    ──────███─█─█─█─████─█───█──█───█───███─███──────
+    ──────█───█───█─█────█───█──█───█───█───█────────
+    ──────███─█───█─█────███─████───█───███─███──────
+    
+    ──────█───█─████─█──█─████─████─███─████─────────
+    ──────██─██─█──█─██─█─█──█─█────█───█──█─────────
+    ──────█─█─█─████─█─██─████─█─██─███─████─────────
+    ──────█───█─█──█─█──█─█──█─█──█─█───█─█──────────
+    ──────█───█─█──█─█──█─█──█─████─███─█─█──────────
+    
+    `)
+
+    promptUser();
+};
+
+// Prompt user for action
+const promptUser = async () => {
+    const answers = await inquirer.prompt([{
+        type: 'list',
+        name: 'choices',
+        message: 'Choose an action',
+        choices: [
+            'VIEW all Departments',
+            'VIEW all Roles',
+            'VIEW all Employees',
+            'EXIT'
+        ]
+    }
+    ]);
+
+     if (answers.choices === 'VIEW all Departments') {
+        await viewAllDepartments();
+    } else if (answers.choices === 'VIEW all Roles') {
+        await viewAllRoles();
+    } else if (answers.choices === 'VIEW all Employees') {
+        await viewAllEmployees();
+    } else if (answers.choices === 'EXIT') {
+        connection.end(); // Close the DB connection
+        console.log('Goodbye!');
+        return;
+    }
+
+};
+
 // Function to view all departments
-function viewAllDepartments() {
-    connection.query('SELECT * FROM department', (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        promptUser(); 
-    });
-}
+const viewAllDepartments = async () => {
+    try {
+        const [results] = await connection.promise().query(
+            'SELECT * FROM department')
+            promptUser();
+        console.table(results)
+    } catch (err) {
+        throw new Error(err)
+    }
+};
 
 // Function to view all roles
-function viewAllRoles() {
-    connection.query('SELECT * FROM role', (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        promptUser(); 
-    });
-}
+const viewAllRoles = async () => {
+    try {
+        const [results] = await connection.promise().query(
+            'SELECT * FROM role')
+            promptUser();
+        console.table(results)
+    } catch (err) {
+        throw new Error(err)
+    }
+};
 
 // Function to view all employees
-function viewAllEmployees() {
-    connection.query('SELECT * FROM employee', (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        promptUser(); 
-    });
-}
+const viewAllEmployees = async () => {
+    try {
+        const [results] = await connection.promise().query(
+            'SELECT * FROM employee')
+            promptUser();
+        console.table(results)
+    } catch (err) {
+        throw new Error(err)
+    }
+};
 
 
 
-// Function to prompt user for action
-function promptUser() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'action',
-                message: 'What would you like to do?',
-                choices: [
-                    'View all departments',
-                    'View all roles',
-                    'View all employees',
-                    'Exit'
-                ],
-            }
-        ])
-        .then(answer => {
-            switch (answer.action) {
-                case 'View all departments':
-                    viewAllDepartments();
-                    break;
-                case 'View all roles':
-                    viewAllRoles();
-                    break;
-                case 'View all employees':
-                    viewAllEmployees();
-                    break;
 
-                case 'Exit':
-                    connection.end(); // Close the DB connection
-                    console.log('Goodbye!');
-                    break;
-            }
-        });
-}
 
 // Start the application 
-promptUser();
+init();
