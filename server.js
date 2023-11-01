d637b26e0fdf04d8edfe5ededf9f39eaed0d93c5
Shocker <zacsmcdowell@gmail.com>
2023-11-01 18:28:47 -0400

    Add functions have been implemented and work

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ const promptUser = async () => {
             'VIEW all Departments',
             'VIEW all Roles',
             'VIEW all Employees',
+            'ADD a Department',
+            'ADD a Role',
+            'ADD an Employee',
             'EXIT'
         ]
     }
@@ -49,6 +52,12 @@ const promptUser = async () => {
         await viewAllRoles();
     } else if (answers.choices === 'VIEW all Employees') {
         await viewAllEmployees();
+    } else if (answers.choices === 'ADD a Department') {
+        await addDepartment();
+    } else if (answers.choices === 'ADD a Role') {
+        await addRole();
+    } else if (answers.choices === 'ADD an Employee') {
+        await addEmployee();
     } else if (answers.choices === 'EXIT') {
         connection.end(); // Close the DB connection
         console.log('Goodbye!');
@@ -92,6 +101,95 @@ const viewAllEmployees = async () => {
         throw new Error(err)
     }
 };
+
+// Function to add a department
+const addDepartment = async () => {
+    try {
+        const answers = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter the department name: '
+            }
+        ]);
+
+        await connection.promise().query('INSERT INTO department SET ?', answers);
+        console.log('Department added successfully!');
+        promptUser();
+    } catch (err) {
+        console.error('Error adding department:', err);
+        promptUser();
+    }
+};
+
+
+// Function to add a role
+const addRole = async () => {
+    try {
+        const answers = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Enter the role name: ',
+            },
+            {
+                type: 'number',
+                name: 'salary',
+                message: 'Enter the salary: ',
+            },
+            {
+                type: 'number',
+                name: 'department_id',
+                message: 'Enter the Department ID#: ',
+            }
+        ]);
+
+        await connection.promise().query('INSERT INTO role SET ?', answers);
+        console.log('Role added successfully!');
+        promptUser();
+    } catch (err) {
+        console.error('Error adding role:', err);
+        promptUser();
+    }
+};
+
+// Function to add an employee
+const addEmployee = async () => {
+    try {
+        const answers = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'First Name: ',
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Last Name: ',
+            },
+            {
+                type: 'number',
+                name: 'role_id',
+                message: 'Enter Job Role ID#: ',
+            },
+            {
+                type: 'number',
+                name: 'manager_id',
+                message: 'Enter the Manager ID#: ',
+            }
+        ]);
+
+        await connection.promise().query('INSERT INTO employee SET ?', answers);
+        console.log('Employee added successfully!');
+        promptUser();
+    } catch (err) {
+        console.error('Error adding employee:', err);
+        promptUser();
+    }
+};
+
+
+
 
 
 // Start the application 
